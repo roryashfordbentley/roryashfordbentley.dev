71414df38f080c83c2441a358a63a2c8d80fb49c
Rory Ashford <rory@superstrukt.co.uk>
2025-01-20 16:59:52 +0000

    Enabled Https for local development (via vite plugin-basic-ssl package)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import react from '@astrojs/react';
-import tinaDirective from "./astro-tina-directive/register"
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
+import tinaDirective from "./astro-tina-directive/register";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: "https://example.com",
 	integrations: [mdx(), sitemap(), react(), tinaDirective()],
+	vite: {
+		plugins: [basicSsl()],
+		server: {
+			https: true,
+		},
+	},
 });
