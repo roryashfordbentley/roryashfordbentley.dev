aa59e4ff823532bba8cc1c74411782628e7510ed
Rory Ashford <rory@superstrukt.co.uk>
2025-01-22 14:18:41 +0000

    Added a custom slugifier for post slugs

--- tina/collections/blog.ts
+++ tina/collections/blog.ts
@@ -1,49 +1,57 @@
 import type { Collection } from "tinacms";
 
 export const BlogCollection: Collection = {
-
-  name: "blog",
-  label: "Blogs",
-  path: "src/content/blog",
-  format: "mdx",
-  ui: {
-    router({ document }) {
-      return `/blog/${document._sys.filename}`;
-    },
-  },
-  fields: [
-    {
-      type: "string",
-      name: "title",
-      label: "Title",
-      isTitle: true,
-      required: true,
-    },
-    {
-      name: "description",
-      label: "Description",
-      type: "string",
-    },
-    {
-      name: "pubDate",
-      label: "Publication Date",
-      type: "datetime",
-    },
-    {
-      name: "updatedDate",
-      label: "Updated Date",
-      type: "datetime",
-    },
-    {
-      name: "heroImage",
-      label: "Hero Image",
-      type: "image",
-    },
-    {
-      type: "rich-text",
-      name: "body",
-      label: "Body",
-      isBody: true,
-    },
-  ],
-}
+	name: "blog",
+	label: "Blogs",
+	path: "src/content/blog",
+	format: "mdx",
+	ui: {
+		filename: {
+			// if disabled, the editor can not edit the filename
+			readonly: true,
+			// Example of using a custom slugify function
+			slugify: (values) => {
+				// Values is an object containing all the values of the form. In this case it is {title?: string, topic?: string}
+				return values?.title?.toLowerCase().replace(/ /g, "-");
+			},
+		},
+		router({ document }) {
+			return `/blog/${document._sys.filename}`;
+		},
+	},
+	fields: [
+		{
+			type: "string",
+			name: "title",
+			label: "Title",
+			isTitle: true,
+			required: true,
+		},
+		{
+			name: "description",
+			label: "Description",
+			type: "string",
+		},
+		{
+			name: "pubDate",
+			label: "Publication Date",
+			type: "datetime",
+		},
+		{
+			name: "updatedDate",
+			label: "Updated Date",
+			type: "datetime",
+		},
+		{
+			name: "heroImage",
+			label: "Hero Image",
+			type: "image",
+		},
+		{
+			type: "rich-text",
+			name: "body",
+			label: "Body",
+			isBody: true,
+		},
+	],
+};
