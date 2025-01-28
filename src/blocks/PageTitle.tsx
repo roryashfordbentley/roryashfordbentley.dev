e3b656512fb2d99d96258cbb91e2f5377c981ce7
Rory Ashford <rory@superstrukt.co.uk>
2025-01-28 19:20:01 +0000

    swapped page title fields to rich-text (wysiwyg)

--- src/blocks/PageTitle.tsx
+++ src/blocks/PageTitle.tsx
@@ -8,11 +8,15 @@ export const PageTitle = ({ data, parentField = "" }) => {
 		<section className="page-title">
 			<div className="page-title__content">
 				<div data-tina-field={tinaField(data, "title")}>
-					<h1 className="page-title__title">{data.title}</h1>
+					<h1 className="page-title__title">
+						<TinaMarkdown content={data.title} />
+					</h1>
 				</div>
 
 				<div data-tina-field={tinaField(data, "subtitle")}>
-					<span className="page-title__subtitle">{data.subtitle}</span>
+					<span className="page-title__subtitle">
+						<TinaMarkdown content={data.subtitle} />
+					</span>
 				</div>
 			</div>
 		</section>
@@ -31,12 +35,12 @@ export const PageTitleBlockSchema: Template = {
 	label: "Page title",
 	fields: [
 		{
-			type: "string",
+			type: "rich-text",
 			name: "title",
 			label: "Title",
 		},
 		{
-			type: "string",
+			type: "rich-text",
 			name: "subtitle",
 			label: "Subtitle",
 		},
