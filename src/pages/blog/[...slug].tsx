4488cf61345f7366d6dfb19d411b50ba83804adf
Rory Ashford <rory@superstrukt.co.uk>
2025-02-06 15:16:16 +0000

    Configured correct page templates

---
+++ src/pages/blog/[...slug].tsx
@@ -0,0 +1,8 @@
+import client from "@tina/__generated__/client";
+import { BlocksLoader } from "@blocks/_BlocksLoader";
+
+export const BlogPost = () => {
+	return <p>coming soon</p>;
+};
+
+export default BlogPost;
