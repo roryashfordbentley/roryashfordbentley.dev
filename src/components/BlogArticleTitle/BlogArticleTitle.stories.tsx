79ffcbce98da7bc8dd8c4f99cdd0815fbd23d222
Rory Ashford <rory@superstrukt.co.uk>
2025-02-17 10:17:46 +0000

    Added Component: Blog Title

---
+++ src/components/BlogArticleTitle/BlogArticleTitle.stories.tsx
@@ -0,0 +1,37 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { BlogArticleTitle } from './BlogArticleTitle'
+import Image from 'next/image'
+
+type Story = StoryObj<typeof meta>
+
+const meta = {
+  title: 'Components/BlogArticleTitle',
+  component: BlogArticleTitle,
+  parameters: {
+    layout: 'padded',
+  },
+} satisfies Meta<typeof BlogArticleTitle>
+
+export default meta
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Core: Story = {
+  args: {
+    title: 'Key changes in WCAG 2.2: What you need to know',
+    description:
+      '2024 saw the official announcement and adoption of WCAG 2.2 which brings a range of additional considerations and updates.',
+    datePostedLabel: 'Posted',
+    datePosted: 'January 6th 2025',
+    readingTimeLabel: 'Reading Time',
+    readingTime: '5 Minutes',
+    featuredImage: (
+      <Image
+        src={'./story-images/climber-1500-844.jpg' as string}
+        alt={'Featured Image Alt Text'}
+        width={1330}
+        height={750}
+      />
+    ),
+  },
+}
--- src/components/BlogGrid/BlogGrid.stories.tsx
+++ src/components/BlogGrid/BlogGrid.stories.tsx
@@ -1,24 +1,24 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from '@storybook/react'
 
-import { BlogGrid } from "./BlogGrid";
-import { CardArticle } from "@components/CardArticle/CardArticle";
-import { Core as CoreCard } from "@components/CardArticle/CardArticle.stories";
+import { BlogGrid } from './BlogGrid'
+import { CardArticle } from '@components/CardArticle/CardArticle'
+import { Core as CoreCard } from '@components/CardArticle/CardArticle.stories'
 
-type Story = StoryObj<typeof meta>;
+type Story = StoryObj<typeof meta>
 
 const meta = {
-	title: "Components/BlogGrid",
-	component: BlogGrid,
-	parameters: {
-		layout: "padded",
-	},
-} satisfies Meta<typeof BlogGrid>;
+  title: 'Components/BlogGrid',
+  component: BlogGrid,
+  parameters: {
+    layout: 'padded',
+  },
+} satisfies Meta<typeof BlogGrid>
 
-export default meta;
+export default meta
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Core: Story = {
-	args: {
-		children: Array(9).fill(<CardArticle {...CoreCard.args} />),
-	},
-};
+  args: {
+    children: Array(9).fill(<CardArticle {...CoreCard.args} />),
+  },
+}
