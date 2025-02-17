d8bc39f7721e10238d52ff59e6a1b749238fc614
Rory Ashford <rory@superstrukt.co.uk>
2025-02-17 14:43:07 +0000

    Configured templates for scaffolding

--- .component_template/{{name}}.stories.tsx
+++ .component_template/{{name}}.stories.tsx
@@ -1,18 +1,18 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
-import { BlogArticleTitle } from './{{ComponentName}}'
+import { BlogArticleTitle } from './{{ name }}'
 import Image from 'next/image'
 
 type Story = StoryObj<typeof meta>
 
 const meta = {
-  title: 'Components/{{ComponentName}}',
-  component: {{ComponentName}},
+  title: 'Components/{{ name }}',
+  component: {{ name }},
   parameters: {
     layout: 'padded',
   },
   tags: ['autodocs'],
-} satisfies Meta<typeof {{ComponentName}}>
+} satisfies Meta<typeof {{ name }}>
 
 export default meta;
 
