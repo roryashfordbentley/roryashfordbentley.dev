0a95778437f9cb230e4c0b349a8c8639dea162cf
Rory Ashford <rory@superstrukt.co.uk>
2025-02-17 14:50:47 +0000

    Added npm script for scaffolding new components

--- .component_template/{{name}}.tsx
+++ .component_template/{{name}}.tsx
@@ -1,33 +1,11 @@
-import { Media } from '@/payload-types'
-import Image from 'next/image'
-import styles from './BlogArticleTitle.module.css' // External CSS for the styles
+import styles from './{{ name }}.module.css'
 
-export const BlogArticleTitle = (props: {
-  title: string
-  description: string
-  datePostedLabel: string
-  datePosted: string
-  readingTimeLabel: string
-  readingTime: string
+export const {{ name }} = (props: {
+  example: string
 }) => {
   return (
-    <section className={styles.grid}>
-      <div className={styles.content}>
-        {props.title && <h1 className={styles.title}>{props.title}</h1>}
-        {props.description && <p className={styles.description}>{props.description}</p>}
-      </div>
-
-      <aside className={styles.meta}>
-        <div className={styles.metaItem}>
-          <span className={styles.metaItemTitle}>{props.datePostedLabel ?? 'Date Posted'}</span>
-          <span className={styles.metaItemValue}>{props.datePosted ?? ''}</span>
-        </div>
-
-        <div className={styles.metaItem}>
-          <span className={styles.metaItemTitle}>{props.readingTimeLabel ?? 'Reading time'}</span>
-          <span className={styles.metaItemValue}>{props.readingTime ?? ''}</span>
-        </div>
-      </aside>
-    </section>
+    <>
+     <p>{props.example}</p>
+    </>
   )
 }
