78cd7d2f37b03b2099b4cd8a94369a6cd623c853
Rory Ashford <rory@superstrukt.co.uk>
2025-02-17 13:57:16 +0000

    Added stories for Card Article

--- src/components/CardArticle/CardArticle.stories.tsx
+++ src/components/CardArticle/CardArticle.stories.tsx
@@ -1,27 +1,28 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from '@storybook/react'
 
-import { CardArticle } from "./CardArticle";
+import { CardArticle } from './CardArticle'
 
-type Story = StoryObj<typeof meta>;
+type Story = StoryObj<typeof meta>
 
 const meta = {
-	title: "Components/CardArticle",
-	component: CardArticle,
-	parameters: {
-		layout: "centered",
-	},
-} satisfies Meta<typeof CardArticle>;
+  title: 'Components/CardArticle',
+  component: CardArticle,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof CardArticle>
 
-export default meta;
+export default meta
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Core: Story = {
-	args: {
-		title: "This is a blog post title, its kinda long",
-		description: "This is the description",
-		date: "07/02/2025",
-		url: "/#test",
-		imageSrc: "./story-images/climber-800-450.jpg",
-		imageAlt: "A rock climber on an indoor boulder problem (V2 in my gym)",
-	},
-};
+  args: {
+    title: 'This is a blog post title, its kinda long',
+    description: 'This is the description',
+    date: '07/02/2025',
+    url: '/#test',
+    imageSrc: './story-images/climber-800-450.jpg',
+    imageAlt: 'A rock climber on an indoor boulder problem (V2 in my gym)',
+  },
+}
