7dccb46d38c9b8115884cbee583f791dbb86edcd
Rory Ashford <rory@superstrukt.co.uk>
2025-02-18 11:20:26 +0000

    Refactored and aded stories to LogoLink component

---
+++ src/components/LogoLink/LogoLink.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { LogoLink } from './LogoLink'
+
+type Story = StoryObj<typeof meta>
+
+const meta = {
+  title: 'Components/LogoLink',
+  component: LogoLink,
+  parameters: {
+    layout: 'padded',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof LogoLink>
+
+export default meta
+
+export const Default: Story = {
+  args: {
+    link: 'https://www.roryashfordbentley.dev',
+  },
+}
