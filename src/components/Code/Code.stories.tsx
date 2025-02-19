134261f45bd3893c3119f80ef994a7b186d9a5df
Rory Ashford <rory@superstrukt.co.uk>
2025-02-19 10:22:25 +0000

    Finished the Code block component

--- src/components/Code/Code.stories.tsx
+++ src/components/Code/Code.stories.tsx
@@ -20,18 +20,58 @@ export const Default: Story = {
     label: 'Code.tsx',
     codeString: `import styles from './Code.module.css'
 import { highlight } from 'sugar-high'
+import { useState } from 'react'
+import { ClipboardDocumentIcon } from '@heroicons/react/24/outline'
+import { ClipboardDocumentCheckIcon } from '@heroicons/react/24/outline'
+import { SunIcon } from '@heroicons/react/24/outline'
+import { MoonIcon } from '@heroicons/react/24/outline'
 
-export const Code = (props: { example: string }) => {
-  const codeHTML = highlight(props.example as string)
+export const Code = (props: { label: string; codeString: string }) => {
+  const codeHTML = highlight(props.codeString as string)
+
+  const [copied, setCopied] = useState(false)
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(props.codeString).then(() => {
+      setCopied(true)
+      setTimeout(() => setCopied(false), 2000)
+    })
+  }
+
+  const [dark, setDark] = useState(true)
+
+  const toggleDarkMode = () => {
+    setDark((prevDark) => !prevDark)
+  }
 
   return (
-    <div>
-      <code
-        dangerouslySetInnerHTML={{ __html: codeHTML }}
-        className={styles.code}
-      />
+    <div className={styles['container--dark']}}>
+      <header className={styles.header}>
+        <div className={styles.circles}>
+          <span className={styles.circle}></span>
+          <span className={styles.circle}></span>
+          <span className={styles.circle}></span>
+        </div>
+
+        {props.label && <div className={styles.title}>{props.label}</div>}
+
+        <div className={styles.buttonGroup}>
+          <button title="Toggle Dark Theme" className={styles.button} onClick={toggleDarkMode}>
+            {dark ? <MoonIcon /> : <SunIcon />}
+          </button>
+
+          <button title="Copy" className={styles.button} onClick={handleCopy}>
+            {copied ? <ClipboardDocumentCheckIcon /> : <ClipboardDocumentIcon />}
+          </button>
+        </div>
+      </header>
+
+      <pre className={styles.codeWrapper}>
+        <code dangerouslySetInnerHTML={{ __html: codeHTML }} className={styles.code} />
+      </pre>
     </div>
   )
-}`,
+}
+`,
   },
 }
