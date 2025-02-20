39c7e6374f4fc6a9a10148facf98ea77aa5f3e55
Rory Ashford <rory@superstrukt.co.uk>
2025-02-20 13:37:47 +0000

    Added autodocs writeup for Prose

--- src/components/Prose/Prose.tsx
+++ src/components/Prose/Prose.tsx
@@ -1,12 +1,18 @@
 import DOMPurify from 'dompurify'
 import styles from './Prose.module.css'
-
 import { ReactNode } from 'react'
 
 function cleanMarkup(htmlCode: string) {
   return { __html: DOMPurify.sanitize(htmlCode) }
 }
 
+/**
+ * The Prose component is used for areas on the site that output rich-text.
+ * This will be used for rendering HTML content provided by CMS or 3rd Party
+ * API's so the component included [DOMPurify](https://github.com/cure53/DOMPurify) to ensure that markup is sanitized
+ * ahead of being passed through to dangerouslySetInnerHTML to prevent XSS
+ * vulnerabilites.
+ */
 export const Prose = ({ children }: { children: string }) => {
   return (
     <section className={styles.contentWrapper} dangerouslySetInnerHTML={cleanMarkup(children)} />
