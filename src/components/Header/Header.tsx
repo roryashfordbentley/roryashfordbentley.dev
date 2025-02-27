17a3ba92b1aa97c2419fd45f3f11e4e1adc998ef
Rory Ashford <rory@superstrukt.co.uk>
2025-02-27 15:49:40 +0000

    Updated header styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -26,15 +26,9 @@ const navItems = navPrimaryData.items;
 export const Header = (props: { logo: ReactNode; navMenu: ReactNode; themeToggle: ReactNode }) => {
   return (
     <header className={styles.wrapper}>
-      <Container>
-        <ContainerItem>
-          <div className={styles.inner}>
-            {props.logo && <div className={styles.logo}>{props.logo}</div>}
-            {props.navMenu && <div className={styles.nav}>{props.navMenu}</div>}
-            {props.themeToggle && <div className={styles.themeToggle}>{props.themeToggle}</div>}
-          </div>
-        </ContainerItem>
-      </Container>
+      {props.logo && <div className={styles.logo}>{props.logo}</div>}
+      {props.navMenu && <div className={styles.nav}>{props.navMenu}</div>}
+      {props.themeToggle && <div className={styles.themeToggle}>{props.themeToggle}</div>}
     </header>
   )
 }
