719a0ca43cd3ef17bc6412913f25dca2dafec342
Rory Ashford <rory@superstrukt.co.uk>
2025-03-04 16:47:03 +0000

    Created a responsive Grid component with minimal CSS

---
+++ src/components/Grid/Grid.stories.tsx
@@ -0,0 +1,108 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Grid, GridItem } from './Grid'
+import { WireframeBox } from '@components/WireframeBox/WireframeBox'
+
+type Story = StoryObj<typeof meta>
+
+const meta = {
+  title: 'Components/Grid',
+  component: Grid,
+  parameters: {
+    layout: 'padded',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof Grid>
+
+export default meta
+
+/**
+ * This is a basic grid with one column. It is the default when no columns are specified.
+ */
+export const SingleColumnGrid: Story = {
+  args: {
+    columns: 1,
+    //gutter: '1rem',
+    children: Array(3).fill(
+      <GridItem>
+        <WireframeBox title="Grid item" />
+      </GridItem>,
+    ),
+  },
+}
+
+/**
+ * This is a 3 column grid example. It will have 3 columns at all screen sizes.
+ */
+export const ThreeColumnGrid: Story = {
+  args: {
+    columns: 3,
+    children: Array(3).fill(
+      <GridItem>
+        <WireframeBox title="Grid item" />
+      </GridItem>,
+    ),
+  },
+}
+
+/**
+ * This example shows how to use the columnSpan props to create a responsive grid
+ * with different numbers of columns at different breakpoints.
+ *
+ * At the smallest size there is 1 column.
+ * At the medium size there are 2 columns.
+ * At the large size there are 4 column.
+ *
+ * Resize the browser window to see how the columns change.
+ */
+export const ResponsiveGrids: Story = {
+  args: {
+    columns: 1,
+    columnsMedium: 2,
+    columnsLarge: 4,
+    children: Array(12).fill(
+      <GridItem>
+        <WireframeBox title="Grid item" />
+      </GridItem>,
+    ),
+  },
+}
+
+/**
+ * This example shows how to use the gutter prop to add a gutter between the grid items.
+ */
+export const Gutters: Story = {
+  args: {
+    columns: 3,
+    gutter: true,
+    children: Array(12).fill(
+      <GridItem>
+        <WireframeBox title="Grid item" />
+      </GridItem>,
+    ),
+  },
+}
+
+/**
+ * This example shows how to use the columnSpan prop on Grid Items to allow grid items to span multiple columns.
+ */
+export const ColumnSpans: Story = {
+  args: {
+    columns: 6,
+    gutter: true,
+    children: [
+      <GridItem columnSpan={6}>
+        <WireframeBox title="6 col" />
+      </GridItem>,
+      <GridItem columnSpan={3}>
+        <WireframeBox title="3 col" />
+      </GridItem>,
+      <GridItem columnSpan={2}>
+        <WireframeBox title="2 col" />
+      </GridItem>,
+      <GridItem columnSpan={1}>
+        <WireframeBox title="1 col" />
+      </GridItem>,
+    ],
+  },
+}
---
+++ src/components/Grid/Grid.tsx
@@ -0,0 +1,44 @@
+import React, { ReactElement } from 'react'
+import styles from './Grid.module.css'
+
+export const Grid = (props: {
+  children: ReactElement<typeof GridItem>[]
+  columns?: number
+  columnsMedium?: number
+  columnsLarge?: number
+  gutter?: boolean
+}) => {
+  const style = {
+    ...(props.columns && { '--columns': props.columns }),
+    ...(props.columnsMedium && { '--columns-m': props.columnsMedium }),
+    ...(props.columnsLarge && { '--columns-l': props.columnsLarge }),
+  }
+
+  return (
+    <div
+      className={`${styles.grid} ${props.gutter ? styles['grid--gutter'] : ''}`}
+      style={style as React.CSSProperties}
+    >
+      {props.children}
+    </div>
+  )
+}
+
+export const GridItem = (props: {
+  children: React.ReactNode
+  columnSpan?: number
+  columnSpanMedium?: number
+  columnSpanLarge?: number
+}) => {
+  const style = {
+    ...(props.columnSpan && { '--col-span': props.columnSpan }),
+    ...(props.columnSpanMedium && { '--col-span-m': props.columnSpanMedium }),
+    ...(props.columnSpanLarge && { '--col-span-l': props.columnSpanLarge }),
+  }
+
+  return (
+    <div className={styles.gridItem} style={style as React.CSSProperties}>
+      {props.children}
+    </div>
+  )
+}
