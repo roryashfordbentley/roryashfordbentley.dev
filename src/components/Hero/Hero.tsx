b0921adc3cf32ebec8bbe586c6c22010d5267223
Rory Ashford <rory@superstrukt.co.uk>
2025-03-17 15:09:41 +0000

    Hero styling update

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -9,8 +9,13 @@ export const Hero = (props: {
   return (
     <section className={styles.wrapper}>
       <header className={styles.header}>
-        <h1 className={styles.title}>{props.title}</h1>
-        <p className={styles.subtitle}>{props.subtitle}</p>
+        {/*<h1 className={styles.title}>{props.title}</h1>*/}
+        <h1 className={styles.title}>
+          Rory <br />
+          Ashford-Bentley
+        </h1>
+        {/*<p className={styles.subtitle}>{props.subtitle}</p>*/}
+        <p className={styles.subtitle}>Senior Full-Stack Engineer</p>
       </header>
 
       <div className={styles.content}>
