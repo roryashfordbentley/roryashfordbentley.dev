3b90a2bc918d3a6b0d8728fd4850bc3e60d3f0af
Rory Ashford <rory@superstrukt.co.uk>
2025-03-24 16:13:27 +0000

    Updated styling of Bluesky feed

--- src/components/BlueskyFeed/BlueskyFeed.tsx
+++ src/components/BlueskyFeed/BlueskyFeed.tsx
@@ -1,9 +1,9 @@
-import AtpAgent, { Agent, CredentialSession } from '@atproto/api'
 import { HeartIcon } from '@heroicons/react/24/outline'
 import { ArrowPathRoundedSquareIcon } from '@heroicons/react/24/outline'
 import { ChatBubbleOvalLeftIcon } from '@heroicons/react/24/outline'
 import { Grid, GridItem } from '@components/Grid/Grid'
 import { Container, ContainerItem } from '@components/Container/Container'
+import { ButtonLink } from '@components/ButtonLink/ButtonLink'
 
 import styles from './BlueskyFeed.module.css' // External CSS for the styles
 
@@ -112,34 +112,41 @@ export async function BlueskyFeed(props: { posts: Array<any> }) {
             <br />
             <em>Bluesky</em>
           </h2>
-        </ContainerItem>
-        <ContainerItem>
-          <Grid columnsMedium={3} gutter>
-            {props.posts.map((post: any, i: number) => {
-              const { author, record, embed, replyCount, repostCount, likeCount } = post.post
+          <div className={styles.posts}>
+            <Grid columnsMedium={3} gutter>
+              {props.posts.map((post: any, i: number) => {
+                const { author, record, embed, replyCount, repostCount, likeCount } = post.post
 
-              const { avatar, displayName, handle } = author
+                const { avatar, displayName, handle } = author
 
-              const content = record.text as string
+                const content = record.text as string
 
-              return (
-                <GridItem key={i}>
-                  <Post
-                    avatar={avatar || ''}
-                    displayName={displayName || ''}
-                    handle={handle}
-                    content={content}
-                    replyCount={replyCount || 0}
-                    repostCount={repostCount || 0}
-                    likeCount={likeCount || 0}
-                    variant={
-                      (i + 1) % 2 == 0 ? 'secondary' : (i + 1) % 3 == 0 ? 'tertiary' : undefined
-                    }
-                  />
-                </GridItem>
-              )
-            })}
-          </Grid>
+                return (
+                  <GridItem key={i}>
+                    <Post
+                      avatar={avatar || ''}
+                      displayName={displayName || ''}
+                      handle={handle}
+                      content={content}
+                      replyCount={replyCount || 0}
+                      repostCount={repostCount || 0}
+                      likeCount={likeCount || 0}
+                      variant={
+                        (i + 1) % 2 == 0 ? 'secondary' : (i + 1) % 3 == 0 ? 'tertiary' : undefined
+                      }
+                    />
+                  </GridItem>
+                )
+              })}
+            </Grid>
+          </div>
+          <div className={styles.buttonWrapper}>
+            <ButtonLink
+              url="https://bsky.app/profile/roikles.bsky.social"
+              label="View more on Bluesky"
+              type="tertiary"
+            />
+          </div>
         </ContainerItem>
       </Container>
     </section>
