43976712681d4aa86f773bb9f2b0372f150402cc
Rory Ashford <rory@superstrukt.co.uk>
2025-03-26 16:44:28 +0000

    Restyled blog title component

--- src/components/BlogArticleTitle/BlogArticleTitle.tsx
+++ src/components/BlogArticleTitle/BlogArticleTitle.tsx
@@ -1,6 +1,11 @@
 import { Media } from '@/payload-types'
 import Image from 'next/image'
+
+import { svgGetContents } from '@scripts/svgGetContents'
+import { InlineSVG } from '@components/InlineSVG/InlineSVG'
+
 import styles from './BlogArticleTitle.module.css' // External CSS for the styles
+import { Container, ContainerItem } from '@components/Container/Container'
 
 export const BlogArticleTitle = (props: {
   title: string
@@ -11,23 +16,51 @@ export const BlogArticleTitle = (props: {
   readingTime?: string
 }) => {
   return (
-    <section className={styles.grid}>
-      <div className={styles.content}>
-        {props.title && <h1 className={styles.title}>{props.title}</h1>}
-        {props.description && <p className={styles.description}>{props.description}</p>}
-      </div>
+    <section className={styles.wrapper}>
+      <header className={styles.inner}>
+        <Container>
+          <ContainerItem>
+            <div className={styles.content}>
+              {props.title && <h1 className={styles.title}>{props.title}</h1>}
+              {props.description && <p className={styles.description}>{props.description}</p>}
+            </div>
 
-      <aside className={styles.meta}>
-        <div className={styles.metaItem}>
-          <span className={styles.metaItemTitle}>{props.datePostedLabel ?? 'Date Posted'}</span>
-          <span className={styles.metaItemValue}>{props.datePosted ?? ''}</span>
-        </div>
+            <aside className={styles.meta}>
+              <div className={styles.metaItem}>
+                <div className={styles.metaIcon}>
+                  <InlineSVG svgPath="/icons/calendar.svg" />
+                </div>
 
-        <div className={styles.metaItem}>
-          <span className={styles.metaItemTitle}>{props.readingTimeLabel ?? 'Reading time'}</span>
-          <span className={styles.metaItemValue}>{props.readingTime ?? ''}</span>
-        </div>
-      </aside>
+                <span className={styles.metaItemValue}>{props.datePosted ?? ''}</span>
+              </div>
+
+              <div className={styles.metaItem}>
+                <div className={styles.metaIcon}>
+                  <InlineSVG svgPath="/icons/book.svg" />
+                </div>
+
+                <span className={styles.metaItemTitle}>
+                  {props.readingTimeLabel ?? 'Reading time'}:
+                </span>
+                <span className={styles.metaItemValue}>{props.readingTime ?? ''}</span>
+              </div>
+            </aside>
+          </ContainerItem>
+        </Container>
+      </header>
+
+      <Container>
+        <ContainerItem>
+          <figure className={styles.coverImage}>
+            <Image
+              src="/story-images/mac-1500-844.jpg"
+              alt="cover image of a Macbook"
+              width={1330}
+              height={720}
+            />
+          </figure>
+        </ContainerItem>
+      </Container>
     </section>
   )
 }
--- src/components/InlineSVG/InlineSVG.tsx
+++ src/components/InlineSVG/InlineSVG.tsx
@@ -6,8 +6,6 @@ export const InlineSVG = (props: { svgPath: string }) => {
   const sanitisedSVGPath = svgGetContents(props.svgPath)
 
   return (
-    <>
-      <figure className={styles.inlineSvg} dangerouslySetInnerHTML={{ __html: sanitisedSVGPath }} />
-    </>
+    <figure className={styles.inlineSvg} dangerouslySetInnerHTML={{ __html: sanitisedSVGPath }} />
   )
 }
