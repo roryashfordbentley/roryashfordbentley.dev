adcb0a696d35b62b42f325a2927dd8fcd0a3b1f1
Rory Ashford <rory@superstrukt.co.uk>
2025-04-03 13:57:30 +0100

    moved icons into public folder

--- src/collections/Icons.ts
+++ src/collections/Icons.ts
@@ -12,31 +12,7 @@ export const Icons: CollectionConfig = {
     },
   ],
   upload: {
-    staticDir: 'icons',
-    imageSizes: [
-      {
-        name: 'thumbnail',
-        width: 400,
-        height: 225,
-        position: 'centre',
-      },
-      {
-        name: 'medium',
-        width: 800,
-        height: undefined,
-        position: 'centre',
-      },
-      {
-        name: 'large',
-        width: 1330,
-        // By specifying `undefined` or leaving a height undefined,
-        // the image will be sized to a certain width,
-        // but it will retain its original aspect ratio
-        // and calculate a height automatically.
-        height: undefined,
-        position: 'centre',
-      },
-    ],
+    staticDir: 'public/icons',
     adminThumbnail: 'thumbnail',
     mimeTypes: ['image/svg+xml'],
   },
