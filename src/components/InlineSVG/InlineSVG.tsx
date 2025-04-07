f4503bc795784305996b14fd8ed613bd4bc38373
Rory Ashford <rory@superstrukt.co.uk>
2025-04-07 10:49:42 +0100

    Fixed type error with innerHTML (fallback to empty string)

--- src/components/InlineSVG/InlineSVG.tsx
+++ src/components/InlineSVG/InlineSVG.tsx
@@ -2,8 +2,10 @@ import { svgGetContents } from '@scripts/svgGetContents'
 
 import styles from './InlineSVG.module.css'
 
-export async function InlineSVG(props: { svgPath: string }) {
+export const InlineSVG = async (props: { svgPath: string }) => {
   const sanitisedSVG = await svgGetContents(props.svgPath)
 
-  return <figure className={styles.inlineSvg} dangerouslySetInnerHTML={{ __html: sanitisedSVG }} />
+  return (
+    <figure className={styles.inlineSvg} dangerouslySetInnerHTML={{ __html: sanitisedSVG || '' }} />
+  )
 }
