a5e7f1bdff286a62499a48d04e1c42bdfa76d7d9
Rory Ashford <rory@superstrukt.co.uk>
2025-04-07 11:17:10 +0100

    Added better path handling for vercel to SVG loader

--- src/scripts/svgGetContents.tsx
+++ src/scripts/svgGetContents.tsx
@@ -1,8 +1,16 @@
 import DOMPurify from 'isomorphic-dompurify'
 
+/**
+ * SVG Get Contents
+ *
+ * @param svgPath The path to the SVG file. This should be a relative path from the public directory.
+ * @returns
+ */
 export async function svgGetContents(svgPath: string): Promise<string | null> {
   try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL || ''}/${svgPath}`)
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_SERVER_URL || `https://${process.env.VERCEL_URL}` || ''}/${svgPath}`,
+    )
 
     if (!res.ok) {
       console.error(`Failed to fetch SVG: ${res.statusText}`)
