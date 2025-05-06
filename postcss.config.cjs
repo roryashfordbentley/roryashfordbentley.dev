0db22c391a36085c5706c1ca6ac26e050a5b1878
Rory Ashford <rory@superstrukt.co.uk>
2025-05-06 16:19:47 +0100

    fixed error in postcss config

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,28 +1,23 @@
 module.exports = {
-	plugins: [
-		[
-			"@csstools/postcss-global-data",
-			{
-				files: ["./src/styles/tokens/media-queries.css"],
-			},
-		],
-		[
-			"postcss-custom-media",
-			{
-				preserve: true,
-			},
-		],
-		[
-			"postcss-rem",
-			{
-				name: "pxrem", // Default to 'rem-convert'
-			},
-		],
-		[
-			"postcss-nesting",
-			{
-				name: "pxrem", // Default to 'rem-convert'
-			},
-		],
-	],
-};
+  plugins: [
+    [
+      '@csstools/postcss-global-data',
+      {
+        files: ['./src/styles/tokens/media-queries.css'],
+      },
+    ],
+    [
+      'postcss-custom-media',
+      {
+        preserve: false, // The preserve option determines whether the original notation is preserved.
+      },
+    ],
+    [
+      'postcss-rem',
+      {
+        name: 'pxrem', // Default to 'rem-convert'
+      },
+    ],
+    ['postcss-nesting', {}],
+  ],
+}
