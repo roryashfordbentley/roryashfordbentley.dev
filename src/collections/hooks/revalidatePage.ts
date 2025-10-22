a05917345a0125810de06c446661606a7457c191
Rory Ashford <roryashfordbentley@gmail.com>
2025-10-22 16:04:05 +0100

    Fixed additional PNPM issues

--- src/collections/hooks/revalidatePage.ts
+++ src/collections/hooks/revalidatePage.ts
@@ -23,7 +23,7 @@ export const revalidatePage: CollectionAfterChangeHook<Page> = ({
       payload.logger.info(`Revalidating page at path: ${path}`)
 
       revalidatePath(path)
-      revalidateTag('pages-sitemap', 'max')
+      revalidateTag('pages-sitemap')
     }
 
     // If the page was previously published, we need to revalidate the old path
@@ -33,7 +33,7 @@ export const revalidatePage: CollectionAfterChangeHook<Page> = ({
       payload.logger.info(`Revalidating old page at path: ${oldPath}`)
 
       revalidatePath(oldPath)
-      revalidateTag('pages-sitemap', 'max')
+      revalidateTag('pages-sitemap')
     }
   }
 
@@ -44,7 +44,7 @@ export const revalidateDelete: CollectionAfterDeleteHook<Page> = ({ doc, req: { 
   if (!context.disableRevalidate) {
     const path = doc?.slug === 'home' ? '/' : `/${doc?.slug}`
     revalidatePath(path)
-    revalidateTag('pages-sitemap', 'max')
+    revalidateTag('pages-sitemap')
   }
 
   return doc
