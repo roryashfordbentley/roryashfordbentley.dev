8c78f92672fb4e7a3b75d0283342e21469dffea4
Rory Ashford <roryashfordbentley@gmail.com>
2025-10-29 11:56:43 +0000

    Remove unused Vercel blob storage

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -61,13 +61,6 @@ export default buildConfig({
   }),
   sharp,
   plugins: [
-    /*vercelBlobStorage({
-      enabled: true,
-      collections: {
-        media: true,
-      },
-      token: process.env.BLOB_READ_WRITE_TOKEN || '',
-    }),*/
     uploadthingStorage({
       collections: {
         media: true,
