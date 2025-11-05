ef7a7f893f0e285160b83858ae439d700a2632b5
Rory Ashford <roryashfordbentley@gmail.com>
2025-11-05 16:35:27 +0000

    Added lexical mock to ToC story

--- src/components/TableOfContents/TableOfContents.stories.tsx
+++ src/components/TableOfContents/TableOfContents.stories.tsx
@@ -2,6 +2,8 @@ import type { Meta, StoryObj } from '@storybook/nextjs'
 
 import { TableOfContents } from './TableOfContents'
 
+import { SerializedEditorState } from '@payloadcms/richtext-lexical/lexical'
+
 type Story = StoryObj<typeof meta>
 
 const meta = {
@@ -13,10 +15,60 @@ const meta = {
   tags: ['autodocs'],
 } satisfies Meta<typeof TableOfContents>
 
-export default meta;
+export default meta
+
+const mockSerializedEditorState: SerializedEditorState = {
+  root: {
+    children: [
+      {
+        type: 'heading',
+        tag: 'h2',
+        format: '',
+        indent: 0,
+        direction: null,
+        version: 1,
+        children: [
+          {
+            text: 'Introduction',
+            type: 'text',
+            detail: 0,
+            format: 0,
+            mode: 'normal',
+            style: '',
+            version: 1,
+          },
+        ],
+      },
+      {
+        type: 'heading',
+        tag: 'h2',
+        format: '',
+        indent: 0,
+        direction: null,
+        version: 1,
+        children: [
+          {
+            text: 'Conclusion',
+            type: 'text',
+            detail: 0,
+            format: 0,
+            mode: 'normal',
+            style: '',
+            version: 1,
+          },
+        ],
+      },
+    ],
+    type: 'root',
+    direction: 'ltr',
+    format: '',
+    indent: 0,
+    version: 1,
+  },
+} as unknown as SerializedEditorState
 
 export const Default: Story = {
   args: {
-    example: 'This is example text',
+    data: mockSerializedEditorState,
   },
 }
